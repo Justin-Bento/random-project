6eaba6a34299333214649af3a1a14443e4bfbe5f
Justin Bento <bento.a.justin@gmail.com>
2023-11-10 12:49:21 -0500

    created a simple navigation bar

--- src/components/MarketingNavbar.jsx
+++ src/components/MarketingNavbar.jsx
@@ -4,16 +4,18 @@ import { Link } from "react-router-dom";
 export default function MarketingNavbar() {
   return (
     <>
-      <header>
-        <img
-          src="/public/assets/marker_logo_black.jpg"
-          alt=""
-          className="h-8 w-auto"
-        />
-        <div>
-          <Link to="/">Home</Link>
-          <Link to="/about">About</Link>
-          <Link to="/conact">Contact</Link>
+      <header className="p-4 bg-gray-50 shadow">
+        <div className="flex flex-col lg:flex-row items-start gap-4 lg:items-center justify-start lg:justify-between">
+          <img
+            src="/assets/logo/marker_rgb_black.svg"
+            alt=""
+            className="h-8 w-auto"
+          />
+          <nav className="flex flex-col lg:flex-row items-center gap-4">
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/conact">Contact</Link>
+          </nav>
         </div>
       </header>
     </>
