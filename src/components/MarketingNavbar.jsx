66cddfac4329219728aa87d7c7b744dd7ff17e2c
Justin Bento <bento.a.justin@gmail.com>
2023-11-10 12:53:10 -0500

    addeed link to home page for the logo

--- src/components/MarketingNavbar.jsx
+++ src/components/MarketingNavbar.jsx
@@ -6,11 +6,13 @@ export default function MarketingNavbar() {
     <>
       <header className="p-4 bg-gray-50 shadow">
         <div className="flex flex-col lg:flex-row items-start gap-4 lg:items-center justify-start lg:justify-between">
-          <img
-            src="/assets/logo/marker_rgb_black.svg"
-            alt=""
-            className="h-8 w-auto"
-          />
+          <Link to="/">
+            <img
+              src="/assets/logo/marker_rgb_black.svg"
+              alt=""
+              className="h-8 w-auto"
+            />
+          </Link>
           <nav className="flex flex-col lg:flex-row items-center gap-4">
             <Link to="/">Home</Link>
             <Link to="/about">About</Link>
