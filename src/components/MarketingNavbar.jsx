ef4acd84fba0fdd6d7838162638956d570d6646c
Justin Bento <bento.a.justin@gmail.com>
2023-11-11 21:46:31 -0500

    Included brand elements to Navbar
    After wrapping the header with a pannel, we included an grid with two
    columns to hold the brand and mobile navigation for the marketing
    website.

--- src/components/MarketingNavbar.jsx
+++ src/components/MarketingNavbar.jsx
@@ -1,12 +1,24 @@
 import { Link } from "react-router-dom";
 // Use Link to navigate to routes
+import { RiMenuLine } from "react-icons/ri";
 
 export default function MarketingNavbar() {
   return (
     <>
       <header className="overflow-hidden rounded-lg bg-white shadow">
         <section className="px-4 py-5 sm:p-6 grid grid-cols-1 lg:grid-cols-2 gap-8">
-          <div className=" h-auto flex items-center justify-between"></div>
+          <div className=" h-auto flex items-center justify-between">
+            <Link to="/">
+              <img
+                src="/assets/logo/marker_rgb_black.svg"
+                alt=""
+                className="h-8 w-auto"
+              />
+            </Link>
+            <div className="border p-2 rounded lg:hidden">
+              <RiMenuLine className="w-4 h-4" />
+            </div>
+          </div>
           <div className=" h-auto"></div>
         </section>
       </header>
