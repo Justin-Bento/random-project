7e30606233c19e1ef70066bf661c7407224732ca
Justin Bento <bento.a.justin@gmail.com>
2023-11-11 23:06:22 -0500

    removed the div and replaced it with a react fragment

--- src/App.jsx
+++ src/App.jsx
@@ -13,14 +13,12 @@ import MarketingNavbar from "./components/MarketingNavbar";
 export default function App() {
   return (
     <>
-      <div>
-        <MarketingNavbar />
-        <Routes>
-          <Route path="/" element={<Home />} />{" "}
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </div>
+      <MarketingNavbar />
+      <Routes>
+        <Route path="/" element={<Home />} />{" "}
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </>
   );
 }
