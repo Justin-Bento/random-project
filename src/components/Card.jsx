bc3d5cc4b786cd06807549bfbe2db7ea08b6c419
Justin Bento <bento.a.justin@gmail.com>
2023-11-12 14:14:27 -0500

    created a component with props and actions

---
+++ src/components/Card.jsx
@@ -0,0 +1,51 @@
+/* eslint-disable react/prop-types */
+import { Link } from "react-router-dom";
+
+export default function Card(props) {
+  return (
+    <div className="bg-gray-50 border rounded transition-all ">
+      <img
+        src={props.Media}
+        alt=""
+        className="w-full h-48 bg-gray-300 rounded-t"
+      />
+      <div className="p-4">
+        <p className="text-xl font-bold capitalize">{props.Title}</p>
+        <p className="text-base mb-4 line-clamp-3 text-gray-600 mt-1">
+          {props.Supporting}
+        </p>
+        <div
+          aria-label="action section for cards"
+          className="flex items-center justify-start gap-4"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            className="w-8 h-8 border p-1 rounded fill-gray-700 hover:border-orange-200 hover:cursor-pointer"
+          >
+            <path d="M6.41421 15.89L16.5563 5.74786L15.1421 4.33365L5 14.4758V15.89H6.41421ZM7.24264 17.89H3V13.6474L14.435 2.21233C14.8256 1.8218 15.4587 1.8218 15.8492 2.21233L18.6777 5.04075C19.0682 5.43128 19.0682 6.06444 18.6777 6.45497L7.24264 17.89ZM3 19.89H21V21.89H3V19.89Z"></path>
+          </svg>
+          <Link
+            to="./bookmark-view"
+            className="border-2 p-1  rounded fill-gray-700 hover:border-orange-200 hover:cursor-pointer"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              className="w-5 h-5"
+            >
+              <path d="M12.0003 3C17.3924 3 21.8784 6.87976 22.8189 12C21.8784 17.1202 17.3924 21 12.0003 21C6.60812 21 2.12215 17.1202 1.18164 12C2.12215 6.87976 6.60812 3 12.0003 3ZM12.0003 19C16.2359 19 19.8603 16.052 20.7777 12C19.8603 7.94803 16.2359 5 12.0003 5C7.7646 5 4.14022 7.94803 3.22278 12C4.14022 16.052 7.7646 19 12.0003 19ZM12.0003 16.5C9.51498 16.5 7.50026 14.4853 7.50026 12C7.50026 9.51472 9.51498 7.5 12.0003 7.5C14.4855 7.5 16.5003 9.51472 16.5003 12C16.5003 14.4853 14.4855 16.5 12.0003 16.5ZM12.0003 14.5C13.381 14.5 14.5003 13.3807 14.5003 12C14.5003 10.6193 13.381 9.5 12.0003 9.5C10.6196 9.5 9.50026 10.6193 9.50026 12C9.50026 13.3807 10.6196 14.5 12.0003 14.5Z"></path>
+            </svg>
+          </Link>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            className="w-8 h-8 border p-1 rounded fill-gray-700 transition-all hover:border-orange-200 hover:bg-red-200 hover:cursor-pointer hover:fill-red-950"
+          >
+            <path d="M17 6H22V8H20V21C20 21.5523 19.5523 22 19 22H5C4.44772 22 4 21.5523 4 21V8H2V6H7V3C7 2.44772 7.44772 2 8 2H16C16.5523 2 17 2.44772 17 3V6ZM18 8H6V20H18V8ZM9 11H11V17H9V11ZM13 11H15V17H13V11ZM9 4V6H15V4H9Z"></path>
+          </svg>
+        </div>
+      </div>
+    </div>
+  );
+}
