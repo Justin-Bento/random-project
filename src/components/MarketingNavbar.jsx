26d66241b40336ea0c29d3754ccea26a606aa2c5
Justin Bento <bento.a.justin@gmail.com>
2023-11-11 22:10:22 -0500

    created all the links to the navigation page

--- src/components/MarketingNavbar.jsx
+++ src/components/MarketingNavbar.jsx
@@ -19,7 +19,38 @@ export default function MarketingNavbar() {
               <RiMenuLine className="w-4 h-4" />
             </div>
           </div>
-          <div className=" h-auto"></div>
+          <div className="flex items-center lg:justify-end gap-4">
+            <Link
+              to="/"
+              className="rounded bg-transparent px-3 py-1.5 text-sm font-semibold text-gray-900 hover:bg-zinc-100 transition-colors"
+            >
+              Home
+            </Link>
+            <Link
+              to="/"
+              className="rounded bg-transparent px-3 py-1.5 text-sm font-semibold text-gray-900 hover:bg-zinc-100 transition-colors"
+            >
+              About
+            </Link>
+            <Link
+              to="/"
+              className="rounded bg-transparent px-3 py-1.5 text-sm font-semibold text-gray-900 hover:bg-zinc-100 transition-colors"
+            >
+              Community
+            </Link>
+            <Link
+              to="/"
+              className="rounded bg-transparent px-3 py-1.5 text-sm font-semibold text-gray-900 hover:bg-zinc-100 transition-colors"
+            >
+              Support us
+            </Link>
+            <Link
+              to="/"
+              className="rounded bg-transparent px-3 py-1.5 text-sm font-semibold text-gray-50 bg-orange-500 hover:bg-orange-500/80 transition-colors"
+            >
+              Get Started
+            </Link>
+          </div>
         </section>
       </header>
     </>
