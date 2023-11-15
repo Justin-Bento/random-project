a522b88ae4ce54f4dda07258832b1b718dec372b
Justin Bento <bento.a.justin@gmail.com>
2023-11-15 00:28:53 -0500

    added a section element to dashboard page

--- src/pages/app/Dashboard.jsx
+++ src/pages/app/Dashboard.jsx
@@ -3,18 +3,20 @@ import { Link } from "react-router-dom";
 export default function Dashboard() {
   return (
     <>
-      <h1>Here is a link to all pages inside the application</h1>
-      <ul>
-        <li>
-          <Link to="account">Account</Link>
-        </li>
-        <li>
-          <Link to="folders">Folders</Link>
-        </li>
-        <li>
-          <Link to="settings">settings</Link>
-        </li>
-      </ul>
+      <section className="min-h-screen py-16 px-8">
+        <h1>Here is a link to all pages inside the application</h1>
+        <ul>
+          <li>
+            <Link to="account">Account</Link>
+          </li>
+          <li>
+            <Link to="folders">Folders</Link>
+          </li>
+          <li>
+            <Link to="settings">settings</Link>
+          </li>
+        </ul>
+      </section>
     </>
   );
 }
