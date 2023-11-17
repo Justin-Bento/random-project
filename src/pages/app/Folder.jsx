d06cd479d9192ccf75d85017b20576bd6a30a878
Justin Bento <bento.a.justin@gmail.com>
2023-11-17 01:51:59 -0500

    grabbed data from modal to add to card

--- src/pages/app/Folder.jsx
+++ src/pages/app/Folder.jsx
@@ -2,50 +2,76 @@
 import { useState } from "react";
 import Card from "../../components/Card";
 import Greeting from "../../components/Greeting"; // Assuming you've extracted Greeting component
+import ModalForm from "../../components/ModalForm"; // Assuming you've extracted Greeting component
 
 // let usersCurrentName = prompt("What is your Name?");
 
 export default function Folder() {
+  const [displayModal, setDisplayModal] = useState(false);
   const [folders, setFolders] = useState([]);
 
-  function createFolder(props) {
+  const openModal = () => {
+    setDisplayModal(true);
+  };
+
+  const closeModal = () => {
+    setDisplayModal(false);
+  };
+
+  const createFolder = (folderData) => {
     setFolders((prevFolders) => [
       ...prevFolders,
-      <Card
-        key={prevFolders.length} // Use a unique key for each folder
-        Title={props.headline || "Hello World"}
-        Supporting={props.supporting || "This is awesome, the card is working"}
-        Link={props.url || "/#"}
-        Media={
-          props.media ||
-          "https://images.unsplash.com/photo-1699116550661-bea051952f96?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        }
-      />,
+      {
+        key: prevFolders.length,
+        Title: folderData.headline || "Hello World",
+        Supporting:
+          folderData.supporting || "This is awesome, the card is working",
+        Link: folderData.url || "/#",
+        Media:
+          folderData.image ||
+          "https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80",
+        // Add other properties you need
+      },
     ]);
-  }
+  };
 
   return (
     <>
-      <section className="overflow-hidden min-h-screen">
-        <div className="px-4 py-5 sm:p-6 lg:py-16 lg:px-8">
-          <div className="flex flex-col lg:flex-row items-center justify-between gap-8">
-            <div>
+      <section className="overflow-hidden min-h-screen mt-16">
+        <div className="container  lg:max-w-9xl mx-auto p-4">
+          <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 ">
+            <div className="">
               <Greeting name={"John"} />
               <p className="text-base tracking-wide text-gray-600">
                 To inject a new element into the DOM on a button click in a
                 React component, you can follow these steps:
               </p>
             </div>
-            <button
-              onClick={createFolder}
-              className="rounded-md bg-orange-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-orange-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-600"
-            >
-              New Folder
-            </button>
+            <div className="flex items-center justify-end">
+              <button
+                className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-zinc-300 transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-zinc-300 focus-visible:ring-zinc-300-2 disabled:pointer-events-none disabled:opacity-50 bg-orange-600 text-orange-50 hover:bg-orange-600/90 h-11 px-4 py-2"
+                onClick={openModal}
+              >
+                Open Modal
+              </button>
+            </div>
           </div>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-8 gap-4">
-            {folders.map((folder, index) => (
-              <div key={index}>{folder}</div>
+
+          <ModalForm
+            displayModal={displayModal}
+            closeModal={closeModal}
+            createFolder={createFolder}
+          />
+          {/* Render your folders using the state */}
+          <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 mt-16">
+            {folders.map((folder) => (
+              <Card
+                key={folder.key}
+                Title={folder.Title}
+                Supporting={folder.Supporting}
+                Link={folder.Link}
+                Media={folder.Media}
+              />
             ))}
           </div>
         </div>
