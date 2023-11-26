e6e38e87fa02da590111062066910803d299a7ab
Justin Bento <bento.a.justin@gmail.com>
2023-11-26 14:43:52 -0500

    updated dashboard too have the title of the url

--- src/pages/app/Dashboard.jsx
+++ src/pages/app/Dashboard.jsx
@@ -1,12 +1,15 @@
 import { Link } from "react-router-dom";
+import { useLocation } from 'react-router-dom';
 
 export default function Dashboard() {
+    // Get the userId param from the URL.
+  let location = useLocation();
   return (
     <>
       <section className="min-h-screen py-16 space-y-16">
         <div className="container mx-auto">
           <div className="">
-            <h1 className="scroll-m-20 border-b capitalize pb-2 text-3xl font-semibold tracking-tight first:mt-0">Here is a link to all pages inside the application</h1>
+            <h1 className="text-3xl font-bold leading-tight tracking-tight text-gray-900 capitalize">{location.pathname.slice(1)}</h1>
           </div>
           <div className="mt-6">
             <ul>
