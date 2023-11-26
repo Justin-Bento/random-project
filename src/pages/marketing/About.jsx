333d4101be02c01254e6f7792ed0511db7366761
Justin Bento <bento.a.justin@gmail.com>
2023-11-26 16:38:54 -0500

    added a minimu height and wrapper to main all pages

--- src/pages/marketing/About.jsx
+++ src/pages/marketing/About.jsx
@@ -1,7 +1,13 @@
 export default function About() {
   return (
     <>
-      <div className="min-h-screen">About</div>
+      <div className="min-h-screen my-16">
+        <div className="wrapper">
+          <h1 className="text-3xl font-bold leading-tight tracking-tight text-gray-900 capitalize">
+            About
+          </h1>
+        </div>
+      </div>
     </>
   );
 }
--- src/pages/marketing/Contact.jsx
+++ src/pages/marketing/Contact.jsx
@@ -1,7 +1,13 @@
 export default function Contact() {
   return (
     <>
-      <div className="min-h-screen">Contact</div>
+      <div className="min-h-screen my-16">
+        <div className="wrapper">
+          <h1 className="text-3xl font-bold leading-tight tracking-tight text-gray-900 capitalize">
+            Contact
+          </h1>
+        </div>
+      </div>
     </>
   );
 }
