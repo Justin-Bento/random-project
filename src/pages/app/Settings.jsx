5723921a4a57c13358ff02bb27855b1fe0dc76fb
Justin Bento <bento.a.justin@gmail.com>
2023-11-27 16:03:06 -0500

    Updated formatting for jsx code

--- src/pages/app/Settings.jsx
+++ src/pages/app/Settings.jsx
@@ -5,9 +5,9 @@ export default function Settings() {
   let location = useLocation();
   return (
     <>
-    <div className="min-h-screen my-16 wrapper">
-      <h1 className='text-3xl font-bold leading-tight tracking-tight text-gray-900 capitalize'>{location.pathname.slice(11)}</h1>
-    </div>
+      <div className="min-h-screen my-16 wrapper">
+        <h1 className='text-3xl font-bold leading-tight tracking-tight text-gray-900 capitalize'>{location.pathname.slice(11)}</h1>
+      </div>
     </>
   );
 }
