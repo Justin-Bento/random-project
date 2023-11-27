594e3342b962090403d71d4c95f5bf8d41ac1107
Justin Bento <bento.a.justin@gmail.com>
2023-11-27 16:03:43 -0500

    Updated formatting for Account page.
    The goal is to stay consistent for the dashboard page and makesure the
    interface aligns with the other pages inside the application.

--- src/pages/app/Account.jsx
+++ src/pages/app/Account.jsx
@@ -4,9 +4,9 @@ export default function Account() {
   let location = useLocation();
   return (
     <>
-      <div className="wrapper min-h-screen py-16 space-y-16">
-        <h1 className="text-3xl font-bold leading-tight tracking-tight text-gray-900 capitalize">{location.pathname.slice(11)}</h1>
+      <div className="min-h-screen my-16 wrapper">
+        <h1 className='text-3xl font-bold leading-tight tracking-tight text-gray-900 capitalize'>{location.pathname.slice(11)}</h1>
       </div>
     </>
-  )
+  );
 }
