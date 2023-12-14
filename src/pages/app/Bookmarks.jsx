730919fdc14f1dbc92ce3c7e4af1960eb862c3fe
Justin Bento <bento.a.justin@gmail.com>
2023-12-13 20:57:32 -0500

    Extracted card title from url path.
    Optimized card title extrations inside Bookmark file by using regex in
    the pathname.

--- src/pages/app/Bookmarks.jsx
+++ src/pages/app/Bookmarks.jsx
@@ -1,9 +1,14 @@
+import { useLocation } from "react-router-dom";
+
 export default function Bookmarks() {
+  const location = useLocation();
+  const crumbs = location.pathname.split(/\/dashboard\/folders\/([^/]+)$/); 
+  let title = crumbs ? crumbs[1] : null;
   return (
     <>
       <div className="xl:container xl:mx-auto my-8 sm:p-6">
         <h1 className="scroll-m-20 pb-2 text-3xl font-bold tracking-normal first:mt-0">
-          Title That Best Suits Bookmarks Category
+          {title} 
         </h1>
         <p className=" leading-7 tracking-wide text-zinc-900">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi esse
